8aadb299e5618b710d0a2201023a338ef4326a04
kmtheobald <kobytheobald@gmail.com>
2020-06-27 06:31:56 +0100

    completed problem using a char list and iterated truncation

---
+++ twenty.py
@@ -0,0 +1,15 @@
+from math import factorial
+
+n = factorial(100)
+num_list = list(str(n))
+for i in range(len(num_list)):
+    num_list[i] = int(num_list[i])
+print(sum(num_list))
+# answer equals 648
+
+m, total = factorial(100), 0
+while (m > 0):
+    total += (m % 10)
+    m = (m // 10)
+print(total)
+# answer equals 648
