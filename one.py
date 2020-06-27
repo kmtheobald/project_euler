c28d06a5e3c0f16e72b0f22a674da39af2a1d51a
kmtheobald <kobytheobald@gmail.com>
2020-06-27 07:27:18 +0100

    remove generalized functions 'multiple'/'main'; changed formatting

--- one.py
+++ one.py
@@ -1,18 +1,9 @@
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
-def multiple(n):
-    sum = 0
-    for i in range(1, n):
-        if (i % 3 == 0) or (i % 5 == 0):
-            sum += i
-    
-    return sum
+n, count = 1000, 0
+for num in range(1, n):
+    if (num % 3 == 0) or (num % 5 == 0):
+            count += num
 
-def main():
-    num = int(input())
-    print(multiple(num))
-
-if __name__ == "__main__":
-    main()
-
-# Input of 1000 yields 233168
+print(count)
+# answer equals 233168
