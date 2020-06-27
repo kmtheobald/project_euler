e12c09495cde2b2e466a24b28d207f3d29ab02c5
kmtheobald <kobytheobald@gmail.com>
2020-06-27 07:34:08 +0100

    removed general functions 'fibonnaci'/'sum_terms'; changed formatting

--- two.py
+++ two.py
@@ -1,23 +1,13 @@
 # By considering the terms of the Fibonacci below four million, 
 # find the sum of the even-valued terms.
 
-def fibonacci(n):
-    list = [1, 2]
-    while ((list[-1] + list[-2]) < n):
-        list.append(list[-1] + list[-2])
-    return list
+n = 4000000
+fib_seq = [1, 2]
+while ((fib_seq[-1] + fib_seq[-2]) < n):
+        fib_seq.append(fib_seq[-1] + fib_seq[-2])
 
-def sum_terms(list):
-    sum = 0
-    for x in range(1, len(list), 3):
-        sum += list[x]
-    return sum
-
-def main():
-    num = int(input())
-    print(sum_terms(fibonacci(num)))
-
-if __name__ == "__main__":
-    main()
-
-# Input of 4000000 yields 4613732
+count = 0
+for even_term in range(1, len(fib_seq), 3):
+    count += fib_seq[even_term]
+print(count)
+# answer equals 4613732
