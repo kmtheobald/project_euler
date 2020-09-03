ea718f2c1297bd6964a1d4ee613de7c9a987d9a1
kmtheobald <kobytheobald@gmail.com>
2020-09-03 01:11:13 -0500

    define problem

---
+++ 17.py
@@ -0,0 +1,2 @@
+# If all the numbers from 1 to 1000 (one thousand) inclusive 
+# were written out in words, how many letters would be used? 
---
+++ 18.py
@@ -0,0 +1,3 @@
+# By starting at the top of the triangle below and moving to adjacent 
+# numbers on the row below ... Find the maximum total from top to 
+# bottom of the [given] triangle
