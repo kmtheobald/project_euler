29cb1b1991fc581690f38baec768fc18d1cf4bda
kmtheobald <kobytheobald@gmail.com>
2020-09-03 01:16:11 -0500

    remove prev unorganized files

--- 16.py
+++
@@ -1,6 +0,0 @@
-# What is the sum of the digits of the number 2^1000?
-
-num = pow(2, 1000)
-num_list = [int(i) for i in str(num)]
-print(sum(num_list))
-# answer equals 1366
--- 17.py
+++
@@ -1,2 +0,0 @@
-# If all the numbers from 1 to 1000 (one thousand) inclusive 
-# were written out in words, how many letters would be used? 
