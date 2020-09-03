f9a4d9494a06af6e2a3473296f3113e04458fd48
kmtheobald <kobytheobald@gmail.com>
2020-09-03 00:37:09 -0500

    initial solve

--- 16.py
+++ 16.py
@@ -1,4 +1,6 @@
 # What is the sum of the digits of the number 2^1000?
 
-def sum_digits(n):
-    
+num = pow(2, 1000)
+num_list = [int(i) for i in str(num)]
+print(sum(num_list))
+# answer equals 1366
