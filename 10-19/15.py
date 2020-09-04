01c8d4420319a3eb2fa04ab77228355f4c023032
kmtheobald <kobytheobald@gmail.com>
2020-09-04 00:47:06 -0500

    initial solve and short explanation

--- 10-19/15.py
+++ 10-19/15.py
@@ -1,3 +1,13 @@
 # How many such routes are there (from top left corner to bottom right corner, 
 # moving only right and down) through a 20×20 grid?
 
+''' Intuitionally and geometrically, the number of routes through a square
+grid can be understood using Pascal's Triangle (albeit 'tilted'). The answer 
+is equivalent to C(2n, n) where n is the dimension of the square grid. '''
+
+from math import comb
+
+n = 20
+print(comb(2*n, n))
+# answer equals 137846528820
+
