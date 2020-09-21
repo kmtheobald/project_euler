03a0cc065ccbcbbd3eed2ac534107f00ab46594a
kmtheobald <kobytheobald@gmail.com>
2020-09-20 22:42:35 -0500

    init solve; while true loop over powers n of i *less than* 10

--- 60-69/63.py
+++ 60-69/63.py
@@ -1,3 +1,18 @@
 # How many n-digit positive integers exist which are also an nth power?
 
+n, i = 1, 1
 
+nth_power_integers = []
+while (True):
+    if (len(str(i**n)) == n):
+        nth_power_integers.append(i**n)
+    i += 1
+    
+    if (len(str(i**n)) > n):
+        n += 1
+        i = 1
+    if (i == 9 and len(str(i**n)) < n):
+        break
+
+print(len(nth_power_integers))
+# answer equals 49
