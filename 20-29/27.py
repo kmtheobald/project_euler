5eb776aa4b2286205427c23de795d6df1ec87bc3
kmtheobald <kobytheobald@gmail.com>
2021-05-20 23:42:23 -0500

    add description

---
+++ 20-29/27.py
@@ -0,0 +1,3 @@
+# Considering quadratics of the form n^2 + an + b where |a| < 1000 and
+# |b| <= 1000, find the product of the a, b pair that produces the maximum 
+# number of primes for consecutive values of n, starting with n = 0.
