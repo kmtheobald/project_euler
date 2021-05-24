243c31e3f8d4bca79a221ea21f7f102ad0367357
kmtheobald <kobytheobald@gmail.com>
2021-05-24 00:09:33 -0500

    init solve with modulus 10**10

--- 40-49/48.py
+++ 40-49/48.py
@@ -1,19 +1,10 @@
 # Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
 
-def self_powers(n):
-    modulo = 10**10
-    sum = 0
-    for i in range(1, n + 1):
-        sum += i**i
-        sum %= modulo
+MODULUS = 10**10
 
-    return sum
-
-def main():
-    num = int(input())
-    print(self_powers(num)) 
-
-if __name__ == "__main__":
-    main()
-
-# for input of 1000, sum = 9110846700
+total = 0
+for i in range(1, 1001):
+    total += pow(i, i)
+        
+print(total % MODULUS)
+# answer equals 9110846700
