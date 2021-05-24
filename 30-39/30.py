920085f07d40e1cd3e1dc17b36eafda428341753
kmtheobald <kobytheobald@gmail.com>
2021-05-23 20:56:01 -0500

    add upper limit const variable

--- 30-39/30.py
+++ 30-39/30.py
@@ -1,8 +1,9 @@
 # Find the sum of all the numbers that can be written as the sum of 
 # fifth powers of their digits.
 
+UPPER_LIMIT = 354294 # 5 * (9^5)
 pent = []
-for num in range(2, 400000):
+for num in range(2, UPPER_LIMIT):
     digits = [int(char) for char in str(num)]
     total = 0
     for digit in digits:
