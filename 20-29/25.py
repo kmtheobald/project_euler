2de8a9d1f537c64b428dca45f4d6e085437dc8f4
kmtheobald <kobytheobald@gmail.com>
2021-06-01 21:09:15 -0500

    init solve

--- 20-29/25.py
+++ 20-29/25.py
@@ -1,2 +1,12 @@
 # What is the index of the first term in the Fibonacci sequence 
 # to contain 1000 digits?
+
+fib_list = [1, 1]
+while(True):
+    fib_next = fib_list[-1] + fib_list[-2]
+    fib_list.append(fib_next)
+    if (len(str(fib_next)) >= 1000):
+        break
+
+print(len(fib_list))
+# answer equals 4782
