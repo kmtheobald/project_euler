7f7dbdb9f0d2941cc43936e7debca586cc96eeff
kmtheobald <kobytheobald@gmail.com>
2021-06-01 20:54:12 -0500

    init solve using prime factorization loop

---
+++ 40-49/47.py
@@ -0,0 +1,21 @@
+# Find the first four consecutive integers to have four distinct prime 
+# factors each. What is the first of these numbers?
+
+from func import factorize
+
+count = 0
+four_prime_factors = []
+for num in range(650, 200000):
+    if (len(factorize(num)) == 4):
+        count += 1
+        four_prime_factors.append(num)
+        if (count == 4):
+            break
+        continue
+    else:
+        count = 0
+        four_prime_factors.clear()
+
+print(four_prime_factors[0])
+# answer equals 134043
+
