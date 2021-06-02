5a0a35d33049f7ca9b6dfab616bac0620b1ece2c
kmtheobald <kobytheobald@gmail.com>
2021-06-02 00:23:09 -0500

    init solve importing func file and is_palindrome function

---
+++ 30-39/36.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.insert(1, os.path.join(sys.path[0], '..'))
+
+# Find the sum of all numbers, less than one million, which are palindromic 
+# in base 10 and base 2.
+
+import func # pylint: disable=E0401
+
+double_palindromes = []
+for num in range(1, 1000000):
+    num_bin = bin(num)[2:]
+    if (func.is_palindrome(num) and \
+        func.is_palindrome(num_bin)):
+        double_palindromes.append(num)
+
+print(sum(double_palindromes))
+# answer equals 872187
+
